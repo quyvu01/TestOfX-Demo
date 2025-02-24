dc244f85261070761166079867a6f332eb6697df
quyvu <quy.vu.0101@gmail.com>
2025-02-24 22:28:02 +0700

    Update proj test

--- Service1/Program.cs
+++ Service1/Program.cs
@@ -1,6 +1,11 @@
+using System.Reflection;
 using Kernel;
+using Microsoft.EntityFrameworkCore;
+using OfX.EntityFrameworkCore.Extensions;
 using OfX.Extensions;
 using OfX.RabbitMq.Extensions;
+using Service1;
+using Service1.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +13,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddOfX(cfg =>
+    {
+        cfg.AddAttributesContainNamespaces(typeof(IKernelAssemblyMarker).Assembly);
+        // cfg.AddRabbitMq(config => config.Host("localhost", "/"));
+        cfg.AddModelConfigurationsFromNamespaceContaining<IAssemblyMarker>();
+        cfg.AddRabbitMq(config => config.Host("localhost", "/"));
+    })
+    .AddOfXEFCore(cfg => cfg.AddDbContexts(typeof(Service1Context), typeof(OtherService1Context)));
+
+builder.Services.AddDbContextPool<Service1Context>(options =>
 {
-    cfg.AddAttributesContainNamespaces(typeof(IKernelAssemblyMarker).Assembly);
-    cfg.AddRabbitMq(config => config.Host("localhost", "/"));
-});
+    options.UseNpgsql("Host=localhost;Username=postgres;Password=Abcd@2021;Database=OfXTestService1", b =>
+    {
+        b.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+        b.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery);
+    });
+}, 128);
+
+builder.Services.AddDbContextPool<OtherService1Context>(options =>
+{
+    options.UseNpgsql("Host=localhost;Username=postgres;Password=Abcd@2021;Database=OfXTestOtherService1", b =>
+    {
+        b.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+        b.UseQuerySplittingBehavior(QuerySplittingBehavior.SingleQuery);
+    });
+}, 128);
 
 builder.Services.AddControllers();
 
